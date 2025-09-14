d794ccb6daa088948305c03521b4cadb3b2a7ff1
Sanna Kramsi <sanna.kramsi@gmail.com>
2025-09-14 15:37:39 +0300

    Renewal of the landing content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://sanna.a11y.ing/',
+  trailingSlash: 'always',
+  integrations: [sitemap({
+    i18n: {
+      defaultLocale: 'en',
+      locales: {
+        en: 'en',
+        fi: 'fi'
+      }
+    }
+  }), tailwind(), icon()],
+});
+
